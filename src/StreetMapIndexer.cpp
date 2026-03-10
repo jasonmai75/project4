a767a7b83e9d8d03130219bc5f13bcaf5121f958
Jason Mai <mrjmai@ucdavis.edu>
2026-03-10 05:36:33 +0000

    struggling to get std::sort to work going to take a break

--- src/StreetMapIndexer.cpp
+++ src/StreetMapIndexer.cpp
@@ -1,13 +1,46 @@
 #include "StreetMapIndexer.h"
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
 
 struct CStreetMapIndexer::SImplementation {
-    std::shared_ptr<CStreetMap> DStreetMap;
-    
-    
+    // Data Storage
+
+    // All nodes from street map sorted by ID (smallest to largest)
+    std::vector<std::shared_ptr<CStreetMap::SNode>> DSortedNodes;
+    // All ways from street map sorted by ID (smallest to largest)
+    std::vector<std::shared_ptr<CStreetMap::SWay>> DSortedWays;
+    // Map node ID to all the ways that can contain that node
+    std::unordered_map<CStreetMap::TNodeID, std::unordered_set<std::shared_ptr<CStreetMap::SWay>>> DWaysByNodeID;
+    // Map node ID to the actual node object (used in WaysInRange)
+    std::unordered_map<CStreetMap::TNodeID, std::shared_ptr<CStreetMap::SNode>> DNodesByID;
+
+    // Build sorted indexes in here
     SImplementation(std::shared_ptr<CStreetMap> streetmap) {
-        DStreetMap = streetmap;
-        // build sorted indexes in here and do stuff
+         // Collect all nodes by index
+         for(std::size_t i = 0; i < streetmap->NodeCount(); i++) {
+            // Get node from position i
+            auto node = streetmap->NodeByIndex(i);
+
+            // Add it to sorted list
+            DSortedNodes.push_back(node);
+
+            // Also store it in our ID->node map for fast lookup later
+            DNodesByID[node->ID()] = node;
+         }
     }
+
+    // Sort nodes by ID so we can return them in a consistent order (lambda can compare two nodes and smaller ID comes first)
+    std::sort(DSortedNodes.begin(), DSortedNodes.end(),
+    [](const std::shared_ptr<CStreetMap::SNode> &a, const std::shared_ptr<CStreetMap::SNode> &b) {
+        return a->ID() < b->ID();
+    });
+
+
+
+
+    
 };
 
 CStreetMapIndexer::CStreetMapIndexer(std::shared_ptr<CStreetMap> streetmap) {
