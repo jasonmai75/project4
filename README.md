dd18e13704378c8b3892c25908b0c9bf071abdd9
Fed19329 <letruonganhkha@gmail.com>
2026-03-10 02:15:38 -0700

    implement non-verbose write plan in texttripplanner

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Student 2: Truong Le (924636822)
 - [std::sort to create sorted Nodes and Ways] (https://en.cppreference.com/w/cpp/algorithm/sort.html)
 - [Data Structure commands and review, specifcally for vectors/maps] (https://www.w3schools.com/cpp/cpp_data_structures.asp)
 - [How to do padding in commandline in C++](https://stackoverflow.com/questions/667183/padding-stl-strings-in-c)
-
+- [How to use std::chronos](https://en.cppreference.com/w/cpp/chrono/duration.html)
 
 Basic git Commands
 git pull --no-rebase origin main
--- src/TextTripPlanWriter.cpp
+++ src/TextTripPlanWriter.cpp
@@ -1,9 +1,15 @@
 #include "TextTripPlanWriter.h"
+#include <iomanip>
+#include <sstream>
 
 struct CTextTripPlanWriter::SImplementation{
 
-    SImplementation(std::shared_ptr<CBusSystem> bussystem){
+    std::shared_ptr<CBusSystem> DBusSystem;
+    std::shared_ptr<SConfig> DConfig;
 
+    SImplementation(std::shared_ptr<CBusSystem> bussystem){
+        DBusSystem = DBusSystem;
+        DConfig = std::make_shared<SConfig>();
     }
 
     ~SImplementation(){
@@ -11,12 +17,62 @@ struct CTextTripPlanWriter::SImplementation{
     }
 
     std::shared_ptr<SConfig> Config() const{
-        
-        return nullptr;
+        return DConfig;
+    }
+
+    std::string FormatTime(CBusSystem::TStopTime stopTime) {
+        auto hours = stopTime.hours().count();
+        auto minutes = stopTime.minutes().count();
+        std::string period = (hours >= 12) ? "PM" : "AM";
+        int displayHour = hours % 12;
+        if(displayHour == 0){
+            displayHour = 12;
+        }
+
+
+        std::ostringstream oss;
+        oss << std::setw(2) << displayHour << ":" 
+            << std::setfill('0') << std::setw(2) << minutes 
+            << " " << period;
+    
+        return oss.str();
     }
 
     bool WritePlan(std::shared_ptr<CDataSink> sink, const TTravelPlan &plan){
-        return false;
+        if(!sink | plan.empty()){
+            return false;
+        }
+        std::ostringstream oss;
+        bool isVerbose = DConfig->FlagEnabled(Verbose);
+        for (size_t i = 0; i < plan.size(); i++)
+        {
+            const auto &step = plan[i];
+            auto stopObj = DBusSystem->StopByID(step.DStopID);
+            auto stopDesc = stopObj ? stopObj->Description() : "Unknown Stop";
+            std::string timeStr = FormatTime(step.DTime);
+
+            if(i == 0){
+                oss << timeStr << ": Take the " << step.DRouteName << " bus from " 
+                << stopDesc << " (stop " << step.DStopID << ").\n";
+            }
+            else if(i == plan.size() - 1){
+                std::string arrivalRoute = plan[i-1].DRouteName;
+                oss << timeStr << ": Get off the " << arrivalRoute << " bus at " 
+                    << stopDesc << " (stop " << step.DStopID << ").\n";
+            }
+            else{
+                std::string prevRoute = plan[i-1].DRouteName;
+                oss << "        : Get off the " << prevRoute << " bus at " << stopDesc 
+                << " (stop " << step.DStopID << ") and wait for the " 
+                << step.DRouteName << " bus.\n";
+                oss << timeStr << ": Take the " << step.DRouteName << " bus from " 
+                << stopDesc << " (stop " << step.DStopID << ").\n";
+            }
+        }
+        std::string finalOutput = oss.str();
+        std::vector<char> outData(finalOutput.begin(), finalOutput.end());
+        sink->Write(outData);
+        return true;
     }
 };
 
